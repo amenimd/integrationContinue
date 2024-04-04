fe4cd43ff5e7b0152456b24b7d2de373eddeac11
amenimd <ameni.mhamed@gmail.com>
2024-04-04 12:25:29 +0200

    generation

---
+++ main.py
@@ -0,0 +1,7 @@
+fichier = open("index.html", "w")
+
+fichier.write ("<html> <body> <h1> site web généré</h1>")
+fichier.write ("<p> ceci est le site du client <b>client1</b></p>")
+fichier.write ("</body></html>")
+
+fichier.close ()
